2c050ff32c7ad86b676c53e43c1fb3d1f0bb7525
Elvin Eduardo <franco193@hotmail.com>
2024-06-28 23:49:20 -0400

    fin del proyecto full stack

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,24 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
-Route::get('/user', function (Request $request) {
+/*Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum')->group(function() {
+    Route::post('logout', [AuthController::class, 'logout']);
+});*/
 
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user', function (Request $request){
+        return $request->user();
+    });
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::apiResource('/users', UserController::class);
+});
+
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
